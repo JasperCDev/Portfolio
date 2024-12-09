f5ee48d125add06926197a9589677e8ced5180a2
JasperCDev <jasperischauvin@gmail.com>
2024-12-08 19:20:20 -0600

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-  return (
-    <Button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      {theme === "light" ? "Dark Mode" : "Light Mode"}
-    </Button>
-  );
+  return "LANDING PAGE";
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,45 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { ChartArea } from "lucide-react";
+
+const items = [
+  {
+    title: "Overview",
+    url: "/dashboard/overview",
+    icon: ChartArea,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
