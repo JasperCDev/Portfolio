01324615d7258b00bcd32e40a6c77f4a3594be3b
JasperCDev <jasperischauvin@gmail.com>
2024-12-14 23:56:16 -0600

    dragging

--- src/app/real-estate/canvas.ts
+++ src/app/real-estate/canvas.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Application,
   Container,
@@ -5,24 +6,53 @@ import {
   FederatedWheelEvent,
   Graphics,
   Point,
+  Rectangle,
+  Ticker,
 } from "pixi.js";
 
-let middleBtnDown = false;
+class Building extends Rectangle {
+  static idHelper = 0;
+  id: string;
+  draggedX: number;
+  draggedY: number;
+  graphics: Graphics;
+  constructor(x: number, y: number, width: number, height: number) {
+    super(x, y, width, height);
+    this.x = x;
+    this.y = y;
+    this.id = (++Building.idHelper).toString();
+    this.draggedX = x;
+    this.draggedY = y;
+    this.graphics = new Graphics().rect(x, y, width, height).stroke("red");
+  }
+}
 
-let pointerPos = new Point();
-let prevPointerPos = new Point();
-let scale = 1;
+let buildings: Array<Building> = [
+  new Building(100, 100, 100, 100),
+  new Building(202, 100, 100, 100),
+  new Building(304, 100, 100, 100),
+  new Building(100, 202, 100, 100),
+  new Building(202, 202, 100, 100),
+  new Building(304, 202, 100, 100),
+];
+
+let leftPointerBtnDown = false;
+let rightPointerBtnDown = false;
+let pointerUp = false;
+let pointerDown = false;
+
+let pointerPos = new Rectangle(0, 0, 10, 10);
+let prevPointerPos = new Rectangle(0, 0, 10, 10);
+let pointerDownPos: Point | null = null;
+
+let focusedElemId: string | null = null;
+let draggingElemId: string | null = null;
+let draggingElemLocalPos: Point | null = null;
 
 export function initCanvas() {
   const app = new Application();
   const root = new Container();
-  const graphics = new Graphics();
-  graphics.rect(100, 100, 100, 100);
-  graphics.rect(200, 200, 100, 100);
-  graphics.rect(300, 300, 100, 100);
-  graphics.rect(400, 400, 100, 100);
-  graphics.fill("red");
-  root.addChild(graphics);
+  root.addChild(...buildings.map((b) => b.graphics));
   app.stage.addChild(root);
   app.init({ background: "black", resizeTo: window }).then(() => {
     document.querySelector("main")!.appendChild(app.canvas);
@@ -31,31 +61,92 @@ export function initCanvas() {
     app.stage.on("pointerdown", handlePointerDown);
     app.stage.on("pointerup", handlePointerUp);
     app.stage.on("pointermove", handlePointerMove);
+    app.stage.on("pointerleave", handlePointerLeave);
     app.stage.on("wheel", handleWheel);
     app.stage.on("contextmenu", (e) => e.preventDefault());
     document.addEventListener("contextmenu", function (event) {
       event.preventDefault();
     });
+
+    Ticker.shared.add(tick);
   });
+
+  function tick() {
+    for (const building of buildings) {
+      console.log(building.graphics.x);
+      let isHover = pointerPos.intersects(building);
+      let isFocus = isHover && leftPointerBtnDown && pointerDown;
+      let isClick = isHover && pointerUp;
+      if (isFocus) {
+        focusedElemId = building.id;
+      }
+      if (focusedElemId === building.id) {
+        let pointerXDiff = pointerDownPos!.x - pointerPos.x;
+        let pointerYDiff = pointerDownPos!.y - pointerPos.y;
+        if (Math.abs(pointerXDiff) >= 3 || Math.abs(pointerYDiff) >= 3) {
+          draggingElemId = building.id;
+          draggingElemLocalPos = new Point(
+            building.graphics.x,
+            building.graphics.y
+          );
+        }
+      }
+      let isDragging = draggingElemId === building.id;
+      let isDragEnd = isDragging && pointerUp;
+      if (isDragging) {
+        const localPointerPos = root.toLocal(
+          pointerPos,
+          building.graphics.parent
+        );
+        // console.log(building.graphics.toGlobal(new Point(0, 0)).x);
+        building.graphics.x = localPointerPos.x;
+        building.graphics.y = localPointerPos.y;
+      }
+      if (isDragEnd) {
+        // building.x = building.graphics.x;
+        // building.y = building.graphics.y;
+      }
+    }
+    pointerUp = false;
+    pointerDown = false;
+  }
 
   function handlePointerDown(e: FederatedPointerEvent) {
     e.preventDefault();
+    pointerDown = true;
+    pointerDownPos = new Point(e.global.x, e.global.y);
+
     switch (e.button) {
+      case 0:
+        leftPointerBtnDown = true;
+        return;
       case 2:
-        middleBtnDown = true;
+        rightPointerBtnDown = true;
         return;
     }
   }
 
   function handlePointerUp() {
-    middleBtnDown = false;
+    rightPointerBtnDown = false;
+    leftPointerBtnDown = false;
+    pointerUp = true;
+    pointerDown = false;
+    draggingElemId = null;
+    draggingElemLocalPos = null;
+    focusedElemId = null;
+    pointerDownPos = null;
+  }
+
+  function handlePointerLeave() {
+    rightPointerBtnDown = false;
+    leftPointerBtnDown = false;
   }
 
   function handlePointerMove(e: FederatedPointerEvent) {
     pointerPos.x = e.global.x;
     pointerPos.y = e.global.y;
 
-    if (middleBtnDown) {
+    if (rightPointerBtnDown) {
       const deltaX = pointerPos.x - prevPointerPos.x;
       const deltaY = pointerPos.y - prevPointerPos.y;
       root.x += deltaX;
@@ -84,4 +175,9 @@ export function initCanvas() {
     root.x -= (localPointerPos.x - root.x) * (clampedScale - oldScale);
     root.y -= (localPointerPos.y - root.y) * (clampedScale - oldScale);
   }
+}
+
+// misc utils
+function derive<T>(cb: () => T extends void ? never : T) {
+  return cb();
 }
